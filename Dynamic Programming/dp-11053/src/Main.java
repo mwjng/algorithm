8a98cccc08297b25a8c77401ec299e349163cccd
mwjng <wmw04087@gmail.com>
2023-03-01 23:51:04 +0900

    .

---
+++ Dynamic Programming/dp-11053/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[1001];
+        int[] d=  new int[1001];
+
+        for(int i = 1; i <= n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        int max = 0;
+
+        for(int i = 1; i <= n; i++) {
+            d[i] = 1;
+            for(int j = 1; j < i; j++) {
+                if(a[i] > a[j] && d[i] < d[j] + 1) {
+                    d[i] = d[j] + 1;
+                }
+            }
+            if(max < d[i])
+                max = d[i];
+        }
+        System.out.println(max);
+    }
+}
