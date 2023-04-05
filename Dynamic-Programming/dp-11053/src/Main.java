99fa851ecd4e672053ec477060bff8f355e7f8b9
mwjng <wmw04087@gmail.com>
2023-04-05 09:55:50 +0900

    .

--- Dynamic-Programming/dp-11053/src/Main.java
+++ Dynamic-Programming/dp-11053/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] a = new int[1001];
-        int[] d=  new int[1001];
+        int[] d = new int[1001];
 
         for(int i = 1; i <= n; i++) {
             a[i] = sc.nextInt();
@@ -16,8 +16,8 @@ public class Main {
         for(int i = 1; i <= n; i++) {
             d[i] = 1;
             for(int j = 1; j < i; j++) {
-                if(a[i] > a[j] && d[i] < d[j] + 1) {
-                    d[i] = d[j] + 1;
+                if(a[i] > a[j]) {
+                    d[i] = Math.max(d[i], d[j] + 1);
                 }
             }
             if(max < d[i])
